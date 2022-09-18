bcd090d612df6ed31ad8374463eb8fb02dca3266
Constanze3 <81444666+Constanze3@users.noreply.github.com>
2022-09-18 13:27:16 +0200

    A simple, clean example

---
+++ Example/Cool.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Cool {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(scanner.nextLine());
+        scanner.close();
+    }
+}
